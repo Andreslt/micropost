5408453c28c73b05c78d35ce91da44d4f273f88f
Andres Lastra <andreslastrat@gmail.com>
2014-04-25 17:16:25 -0400

    initial commit

---
+++ .bash_history
@@ -0,0 +1,14 @@
+ruby -v
+gem -v
+rails -v
+rvm gemset list
+rails new micropost
+cd micropost
+subl
+exit
+bundle
+exit
+bundle
+exit
+bundle
+exit
---
+++ micropost/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Micropost::Application.config.secret_token = '524d6ea16043baa2489449af12654925239db43d889dec5972024a303e1d148a52f94e94e0ff44bd0dfbb0e35739a218254b906b3e63529fbd84844192054101'
